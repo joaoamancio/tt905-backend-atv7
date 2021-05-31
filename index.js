657d240b51ba8dabb4ba3a87254c89f9200d69d6
joaoamancio <j218833@dac.unicamp.br>
2021-05-31 17:16:50 -0300

    add um endpoint

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 //
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+// Permissões
+var cors = require('cors');
+app.use(cors());
+
+// Porta que eu estou ouvindo
+app.listen(process.env.PORT || 3000);
+
+app.get('/', 
+    function (req, res){    
+        res.send("Hello World");
+    }
+);
+
+app.get('/hello',
+function (req, res){    
+    res.send("Hello de Novo");
+    }
+)
