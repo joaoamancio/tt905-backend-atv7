3785146b5ac999d148d11f6ecdbae57c4d3c0c32
joaoamancio <j218833@dac.unicamp.br>
2021-06-21 00:58:11 -0300

    atualizadoagora

--- index.js
+++ index.js
@@ -83,18 +83,6 @@ app.delete('/books/title/:id',
         res.send("Título deletado com sucesso");
     }
 );
-/*
-jeadC7ibjecNsFKr
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://admin:<password>@cluster0.estq0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-*/
 
 // Utilizando MongoDB
 
